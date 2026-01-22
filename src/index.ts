01bf9159b572743ad319679b4fc81ed54e62e1ae
sarthak <sarthakssingh49@gmail.com>
2026-01-23 00:42:33 +0530

    Basic types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-// const variable: string = "Hello from Typescript";
-// const word: string = "this is from changes";
-// console.log(word)
+const variable: string = "Hello from Typescript";
+const word: string = "this is from changes";
+//console.log(word)
 
-// console.log(variable);
+console.log(variable);
 
 const add = (num1: number, num2: number): number => {
     return num1+num2;
 }
 
 console.log(add(56, 45))
+
+type User = {
+    name: string,
+    // age can be ignored
+    age: number
+}
+
+type Person = {
+    height: number,
+    weight: number
+}
+
+type worker = User | Person;
+
+const letsgo: worker = {
+    height: 56,
+    weight: 56
+}
+
+console.log(letsgo.height);
+
+let arr: number[] = [1, 2, 56, 34];
+
+let num: any;
+
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
